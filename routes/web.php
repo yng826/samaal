def812ed7178eccf3fc037edb23b9a10dc17bdec
hyuk <yng826@gmail.com>
2020-11-10 01:24:37 +0900

    controller, view 추가

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\aboutUs\HeritageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('about-us')->group(function() {
+    // /about-us/heriatage
+    Route::get('heritage', [HeritageController::class, 'index']);
 });
