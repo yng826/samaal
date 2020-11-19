175737316f52866883747add67dadc7778e9c071
akaming <audals2478@gmail.com>
2020-11-20 00:01:35 +0900

    footer 추가

--- resources/views/aboutUs/aboutCi.blade.php
+++ resources/views/aboutUs/aboutCi.blade.php
@@ -68,70 +68,72 @@
         </main>
 
         <footer class="footer">
-            <h1 class="footer__logo">SAMA</h1>
-            <div class="footer__nav">
-                <ul>
-                    <li>
-                        <h3>About Us</h3>
-                        <ul>
-                            <li><a href="#">Heritage</a></li>
-                            <li><a href="#">Message</a></li>
-                            <li><a href="#">Story & News</a></li>
-                            <li><a href="#">재무정보</a></li>
-                            <li><a href="#">Location</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <h3>For Business Partners</h3>
-                        <ul>
-                            <li><a href="#">알루미늄 호일</a></li>
-                            <li><a href="#">포장재</a></li>
-                            <li><a href="#">산업/건축용</a></li>
-                            <li><a href="#">Speciality</a></li>
-                            <li><a href="#">Innovation</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <h3>Work With Us</h3>
-                        <ul>
-                            <li><a href="#">채용공고</a></li>
-                            <li><a href="#">직무소개</a></li>
-                            <li><a href="#">FAQ</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <h3>Others</h3>
-                        <ul>
-                            <li><a href="#">사이트맵</a></li>
-                            <li><a href="#">통합검색</a></li>
-                            <li><a href="#">통합문의</a></li>
-                            <li><a href="#">약관 및 정책</a></li>
-                        </ul>
-                    </li>
-                </ul>
+            <div class="footer__wrap">
+                <h1 class="footer__logo">SAMA</h1>
+                <div class="footer__nav">
+                    <ul class="footer__nav-list">
+                        <li class="footer__nav-list--item">
+                            <h3>About Us</h3>
+                            <ul>
+                                <li><a href="#">Heritage</a></li>
+                                <li><a href="#">Message</a></li>
+                                <li><a href="#">Story & News</a></li>
+                                <li><a href="#">재무정보</a></li>
+                                <li><a href="#">Location</a></li>
+                            </ul>
+                        </li>
+                        <li class="footer__nav-list--item">
+                            <h3>For Business Partners</h3>
+                            <ul>
+                                <li><a href="#">알루미늄 호일</a></li>
+                                <li><a href="#">포장재</a></li>
+                                <li><a href="#">산업/건축용</a></li>
+                                <li><a href="#">Speciality</a></li>
+                                <li><a href="#">Innovation</a></li>
+                            </ul>
+                        </li>
+                        <li class="footer__nav-list--item">
+                            <h3>Work With Us</h3>
+                            <ul>
+                                <li><a href="#">채용공고</a></li>
+                                <li><a href="#">직무소개</a></li>
+                                <li><a href="#">FAQ</a></li>
+                            </ul>
+                        </li>
+                        <li class="footer__nav-list--item">
+                            <h3>Others</h3>
+                            <ul>
+                                <li><a href="#">사이트맵</a></li>
+                                <li><a href="#">통합검색</a></li>
+                                <li><a href="#">통합문의</a></li>
+                                <li><a href="#">약관 및 정책</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer__inner">
+                    <div class="droplist-box">
+                        <button class="droplist-box__button">패밀리사이트</button>
+                        <!-- <ul class="droplist">
+                            <li id="" class="droplist__item"><a href="#">패밀리1</a></li>
+                            <li id="" class="droplist__item"><a href="#">패밀리2</a></li>
+                            <li id="" class="droplist__item"><a href="#">패밀리3</a></li>
+                        </ul> -->
+                    </div>
+                    <div class="droplist-box">
+                        <button class="droplist-box__button">개인정보처리방침</button>
+                        <!-- <ul class="droplist">
+                            <li id="" class="droplist__item"><a href="#">개인정보</a></li>
+                            <li id="" class="droplist__item"><a href="#">패밀리2</a></li>
+                            <li id="" class="droplist__item"><a href="#">패밀리3</a></li>
+                        </ul> -->
+                    </div>
+                    <div class="question-btn">
+                        <a href="#">문의하기</a>
+                    </div>
+                </div>
+                <p class="copyright">ⓒ 2020 SAMA. ALL RIGHTS RESERVED. DESIGNED BY DOMOBRODEUR</p>
             </div>
-            <div>
-                <div class="droplist-box">
-					<button class="droplist-box__button">패밀리사이트</button>
-					<!-- <ul class="droplist">
-						<li id="" class="droplist__item"><a href="#">패밀리1</a></li>
-						<li id="" class="droplist__item"><a href="#">패밀리2</a></li>
-						<li id="" class="droplist__item"><a href="#">패밀리3</a></li>
-					</ul> -->
-                </div>
-                <div class="droplist-box">
-					<button class="droplist-box__button">개인정보처리방침</button>
-					<!-- <ul class="droplist">
-						<li id="" class="droplist__item"><a href="#">개인정보</a></li>
-						<li id="" class="droplist__item"><a href="#">패밀리2</a></li>
-						<li id="" class="droplist__item"><a href="#">패밀리3</a></li>
-					</ul> -->
-                </div>
-                <div class="question-btn">
-                    <a href="#">문의하기</a>
-                </div>
-            </div>
-            <p class="copyright">ⓒ 2020 SAMA. ALL RIGHTS RESERVED. DESIGNED BY DOMOBRODEUR</p>
         </footer>
     </body>
 </html>
