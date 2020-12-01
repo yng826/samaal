b9520d6d28b62928a9a246ccf374941290a2c679
hyuk <yng826@gmail.com>
2020-12-02 00:45:22 +0900

    require 누락

--- webpack.frontend.mix.js
+++ webpack.frontend.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-merge-manifest');
 
 /*
  |--------------------------------------------------------------------------
@@ -18,9 +19,9 @@ mix
     .js('resources/js/works/business.js', 'public/js')
     .js('resources/js/works/question.js', 'public/js')
     .extract(['vue','jquery'])
+    .mergeManifest ()
     .autoload({
         jquery: ['$', 'jQuery', 'jquery'],
     })
-    .mergeManifest ()
     .sass('resources/sass/vendor.scss', 'public/css')
     .sass('resources/sass/app.scss', 'public/css');
