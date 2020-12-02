11ba006e34d8cd933d4aa54e3494127d362a3bef
hyuk <yng826@gmail.com>
2020-12-03 02:50:34 +0900

    헤더 include

--- resources/views/layouts/business.blade.php
+++ resources/views/layouts/business.blade.php
@@ -5,58 +5,7 @@
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body class="business">
-        <!-- {{-- @section('sidebar')
-            This is the master sidebar.
-        @show
-
-        <div class="container">
-            @yield('content')
-        </div> --}} -->
-        <header class="header">
-            <a href="#" class="header__logo">
-                <img src="/images/common/logo.png" alt="SAMA">
-            </a>
-            <nav class="header__nav">
-                <ul>
-                    <li class="header__nav--item">
-                        <span>About Us</span>
-                        <ol class="header__nav--submenu">
-                            <li><a href="#">Story & News</a></li>
-                            <li><a href="#">Heritage</a></li>
-                            <li><a href="#">Location</a></li>
-                            <li><a href="#">Investor Relations</a></li>
-                        </ol>
-                    </li>
-                    <li class="header__nav--item">
-                        <span>For Business Partners</span>
-                        <ol class="header__nav--submenu">
-                            <li><a href="#">알루미늄 호일</a></li>
-                            <li><a href="#">포장재용</a></li>
-                            <li><a href="#">산업/건축용</a></li>
-                            <li><a href="#">Speciality</a></li>
-                            <li><a href="#">Innovation</a></li>
-                        </ol>
-                    </li>
-                    <li class="header__nav--item">
-                        <span>Work With Us</span>
-                        <ol class="header__nav--submenu">
-                            <li><a href="#">채용안내</a></li>
-                            <li><a href="#">인사제도</a></li>
-                            <li><a href="#">조직문화</a></li>
-                        </ol>
-                    </li>
-                </ul>
-            </nav>
-            <div class="header__search">
-                <ul>
-                    <li class="language__kor"><a href="#">KOR</a></li>
-                    <li class="language__eng"><a href="#">ENG</a></li>
-                </ul>
-                <!-- <input type="text"> -->
-                <button type="submit" class="btn-search">검색</button>
-            </div>
-        </header>
-
+        @include('shared.header')
         <main class="business-detail contents-wrap">
             <div class="contents-wrap__title business-detail__title">
                 <h2>
---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,45 @@
+
+<header class="header">
+    <a href="#" class="header__logo">
+        <img src="/images/common/logo.png" alt="SAMA">
+    </a>
+    <nav class="header__nav">
+        <ul>
+            <li class="header__nav--item">
+                <span>About Us</span>
+                <ol class="header__nav--submenu">
+                    <li><a href="#">Story & News</a></li>
+                    <li><a href="#">Heritage</a></li>
+                    <li><a href="#">Location</a></li>
+                    <li><a href="#">Investor Relations</a></li>
+                </ol>
+            </li>
+            <li class="header__nav--item">
+                <span>For Business Partners</span>
+                <ol class="header__nav--submenu">
+                    <li><a href="#">알루미늄 호일</a></li>
+                    <li><a href="#">포장재용</a></li>
+                    <li><a href="#">산업/건축용</a></li>
+                    <li><a href="#">Speciality</a></li>
+                    <li><a href="#">Innovation</a></li>
+                </ol>
+            </li>
+            <li class="header__nav--item">
+                <span>Work With Us</span>
+                <ol class="header__nav--submenu">
+                    <li><a href="#">채용안내</a></li>
+                    <li><a href="#">인사제도</a></li>
+                    <li><a href="#">조직문화</a></li>
+                </ol>
+            </li>
+        </ul>
+    </nav>
+    <div class="header__search">
+        <ul>
+            <li class="language__kor"><a href="#">KOR</a></li>
+            <li class="language__eng"><a href="#">ENG</a></li>
+        </ul>
+        <!-- <input type="text"> -->
+        <button type="submit" class="btn-search">검색</button>
+    </div>
+</header>
