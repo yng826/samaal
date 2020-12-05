2dd5b98c068a19db684ccb232aa0d804520f64f8
hyuk <yng826@gmail.com>
2020-12-06 01:46:44 +0900

    기타등등

--- resources/views/workWithUs/recruit/show.blade.php
+++ resources/views/workWithUs/recruit/show.blade.php
@@ -6,5 +6,14 @@
         <div>
             <h4>{{ $recruit->title }}</h4>
         </div>
+        <div class="button-group">
+            <a href="#">입사지원</a>
+            <a href="#">입사지원 확인</a>
+        </div>
     </div>
+@endsection
+
+@section('script')
+    @parent
+    <script src="{{ asset('js/recruit.js') }}" defer></script>
 @endsection
