6a403ebd1892877b05cbc8b759f08a41fa65e07c
akaming <audals2478@gmail.com>
2020-12-06 22:10:08 +0900

    스토리뉴스 오타 수정

--- resources/views/aboutUs/storyNews.blade.php
+++ resources/views/aboutUs/storyNews.blade.php
@@ -17,7 +17,7 @@ $bodyClass = 'about';
                 <li class="about-storyNews__list-item">
                         <a href="javascript:;" class="news-btn" id="news-btn-{{ $info->id }}">
                         <input type="hidden"  value="{{ $info->id }}">
-                        <figure style="background-image:url(http://placeimg.com/640/480/animals);>
+                        <figure style="background-image:url(http://placeimg.com/640/480/animals);">
                             {{-- <img src="/storage/{{ $info->img_file_path }}" alt=""> --}}
                         </figure>
                         <div class="about-storyNews__list-item--text">
