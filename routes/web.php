4f6f3b7f689e7d88cafa0ef270ca180ad88c37a4
leeyena1371 <leeyena1371@kakao.com>
2020-12-06 18:08:39 +0900

    어드민화면 : ir_board 리스트에 내용 삭제 / 사용자 화면 :뉴스 메인 CSS추가

--- routes/web.php
+++ routes/web.php
@@ -214,6 +214,11 @@ Route::prefix('faq')->group(function () {
 });
 
 
+Route::prefix('admin')->group(function () {
+    Route::get('ir_board/file-download', [IrBoardController::class, 'fileDownload']);
+    Route::get('news_info/file-download', [NewsInfoController::class, 'fileDownload']);
+
+});
 Route::get('role', function () {
     return 'auth';
 })->middleware(['roles:admin']);
@@ -231,10 +236,10 @@ Route::prefix('admin')->middleware(['auth', 'roles:admin'])->group(function () {
 });
 
 Route::prefix('admin')->middleware('auth')->group(function () {
-    Route::get('ir_board/file-download', [IrBoardController::class, 'fileDownload']);
+    //Route::get('ir_board/file-download', [IrBoardController::class, 'fileDownload']);
     Route::resource('ir_board', Admin\IrBoardController::class);
 
-    Route::get('news_info/file-download', [NewsInfoController::class, 'fileDownload']);
+    //Route::get('news_info/file-download', [NewsInfoController::class, 'fileDownload']);
     Route::resource('news_info', Admin\NewsInfoController::class);
 
     Route::get('iso_certification/file-download', [IsoCertificationController::class, 'fileDownload']);
