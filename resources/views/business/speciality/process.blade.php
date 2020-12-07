aa1e97422d3f21e9402a7eb24bfe6c76563220e6
akaming <audals2478@gmail.com>
2020-12-07 23:33:20 +0900

    공정과정  web1600 사이즈 추가

--- resources/views/business/speciality/process.blade.php
+++ resources/views/business/speciality/process.blade.php
@@ -17,34 +17,34 @@
                 <li data-id="content-03" class="speciality-process__tab--item">알루미늄페이스트</li>
             </ul>
         </div>
-        <div class="content-01 speciality-process__tab-content on">
-            <p class="content-01__text">
+        <div class="content-01 speciality-process__tab-content on process-layout">
+            <p class="process-layout__text">
                 삼아알미늄은 최고의 압연 기술력을 바탕으로<br>
                 앞으로도 꾸준히 고객과 함께 업계의 리더로서 책임을 다하겠습니다.
             </p>
-            <ul class="content-01__list">
-                <li class="content-01__list--item">
+            <ul class="process-layout__list">
+                <li class="process-layout__list--item content-01__list--item">
                     <span class="lang-korea">압연</span>
                     <stron class="lang-english">Roughing</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
                 </li>
-                <li class="content-01__list--item">
+                <li class="process-layout__list--item content-01__list--item">
                     <span class="lang-korea">분리, 분단</span>
                     <stron class="lang-english">Separating/Slitting</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
                 </li>
-                <li class="content-01__list--item">
+                <li class="process-layout__list--item content-01__list--item">
                     <span class="lang-korea">연화</span>
                     <stron class="lang-english">Annealing</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
                 </li>
-                <li class="content-01__list--item">
+                <li class="process-layout__list--item content-01__list--item">
                     <span class="lang-korea">포장</span>
                     <stron class="lang-english">Packaging</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
                 </li>
             </ul>
-            <p class="content-01__text">
+            <p class="process-layout__text">
                 세계 최대 작업 폭 2,100mm의 최첨단 압연기를 비롯<br>
                 분리기, 분단기, 핀홀 검사기 등의 최신 생산설비<br>
                 <span>국내 최초로 4.5㎛ 중합 압연 성공</span>
@@ -54,34 +54,34 @@
             <div class="content-01__img"></div>
         </div>
 
-        <div class="content-02 speciality-process__tab-content">
-            <p class="content-02__text">
+        <div class="content-02 speciality-process__tab-content process-layout">
+            <p class="process-layout__text">
                 1969년 담배포장용 알루미늄박지 생산을 시작한 이래 일반 식품포장재에서<br>
                 제약, 건축, 산업용 포장재에 이르는 다양한 포장분야에서 뛰어난 품질과 개발능력으로 업계를 선도하고 있습니다.
             </p>
-            <ul class="content-02__list">
-                <li class="content-02__list--item">
+            <ul class="process-layout__list">
+                <li class="process-layout__list--item content-02__list--item">
                     <span class="lang-korea">인쇄기</span>
                     <stron class="lang-english">Printing</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
                 </li>
-                <li class="content-02__list--item">
+                <li class="process-layout__list--item content-02__list--item">
                     <span class="lang-korea">라미네이터</span>
                     <stron class="lang-english">Laminator</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
                 </li>
-                <li class="content-02__list--item">
+                <li class="process-layout__list--item content-02__list--item">
                     <span class="lang-korea">분단기</span>
                     <stron class="lang-english">Slitting</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
                 </li>
-                <li class="content-02__list--item">
+                <li class="process-layout__list--item content-02__list--item">
                     <span class="lang-korea">제대기</span>
                     <stron class="lang-english">Bag Making</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
                 </li>
             </ul>
-            <p class="content-02__text">
+            <p class="process-layout__text">
                 인쇄기, 라미네이터, 제대기를 비롯한 최신의 첨단화된 가공설비를 도입<br>
                 <span>
                     국내 최초로 레토르트 파우치 및 제약용 Alu-Alu Cold Forming Foil, 리필 파우치,<br>
@@ -92,34 +92,34 @@
             </p>
             <div class="content-02__img"></div>
         </div>
-        <div class="content-03 speciality-process__tab-content">
-            <p class="content-03__text">
+        <div class="content-03 speciality-process__tab-content process-layout">
+            <p class="process-layout__text">
                 1974년 국내 최초로 알루미늄 페이스트 생산 시작.<br>
                 알루미늄박 생산과정에서 발생하는 SCRAP에 가치를 부가하여 수입대체는 물론<br>
                 선박, 건설, 자동차 등 산업전반의 발전에 기여하고 있습니다
             </p>
-            <ul class="content-03__list">
-                <li class="content-03__list--item">
+            <ul class="process-layout__list">
+                <li class="process-layout__list--item content-03__list--item">
                     <span class="lang-korea">분쇄</span>
                     <stron class="lang-english">smash</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
                 </li>
-                <li class="content-03__list--item">
+                <li class="process-layout__list--item content-03__list--item">
                     <span class="lang-korea">분급</span>
                     <stron class="lang-english">classification</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
                 </li>
-                <li class="content-03__list--item">
+                <li class="process-layout__list--item content-03__list--item">
                     <span class="lang-korea">오일제거</span>
                     <stron class="lang-english">fliter</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
                 </li>
-                <li class="content-03__list--item">
+                <li class="process-layout__list--item content-03__list--item">
                     <span class="lang-korea">혼합</span>
                     <stron class="lang-english">Mixer</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
                 </li>
-                <li class="content-03__list--item">
+                <li class="process-layout__list--item content-03__list--item">
                     <span class="lang-korea">포장</span>
                     <stron class="lang-english">Packagin</stron>
                     <a href="#" class="popup-btn">동영상 보기</a>
