1e675174a6ba78c3de9927efb7ecc992edf2649c
leeyena1371 <leeyena1371@kakao.com>
2020-12-08 21:41:39 +0900

    어드민 재무정보관리 수정/ IR공고관리 수정

--- resources/views/admin/finance_info/create.blade.php
+++ resources/views/admin/finance_info/create.blade.php
@@ -30,44 +30,44 @@
                             <input type="hidden" class="form-control" name="info_year" value="{{$info->info_year ?? $new_info_year}}">
                         </div>
                         <div class="form-group">
-                            <label for="">연결_매출액</label>
+                            <label for="">연결_매출액 <p style="display: inline-block;font-size: 70%;">(단위: 백만원)</p></label>
                             <input type="text" class="form-control" id="connect_sales" name="connect_sales" value="{{$info->connect_sales ?? ''}}">
                         </div>
                         <div class="form-group">
-                            <label for="">연결_영업이익</label>
+                            <label for="">연결_영업이익 <p style="display: inline-block;font-size: 70%;">(단위: 백만원)</p></label>
                             <input type="text" class="form-control" id="connect_operating_income" name="connect_operating_income" value="{{$info->connect_operating_income ?? ''}}">
                         </div>
                         <div class="form-group">
-                            <label for="">연결_당기순이익</label>
+                            <label for="">연결_당기순이익 <p style="display: inline-block;font-size: 70%;">(단위: 백만원)</p></label>
                             <input type="text" class="form-control" id="connect_net_income" name="connect_net_income" value="{{$info->connect_net_income ?? ''}}">
                         </div>
                         <div class="form-group">
-                            <label for="">연결_자산</label>
+                            <label for="">연결_자산 <p style="display: inline-block;font-size: 70%;">(단위: 백만원)</p></label>
                             <input type="text" class="form-control" id="connect_assets" name="connect_assets" value="{{$info->connect_assets ?? ''}}">
                         </div>
                         <div class="form-group">
-                            <label for="">연결_부채</label>
+                            <label for="">연결_부채 <p style="display: inline-block;font-size: 70%;">(단위: 백만원)</p></label>
                             <input type="text" class="form-control" id="connect_liability" name="connect_liability" value="{{$info->connect_liability ?? ''}}">
                         </div>
                         <div class="form-group">
-                            <label for="">별도_매출액</label>
+                            <label for="">별도_매출액 <p style="display: inline-block;font-size: 70%;">(단위: 백만원)</p></label>
                             <input type="text" class="form-control" id="separate_sales" name="separate_sales" value="{{$info->separate_sales ?? ''}}">
                         </div>
                         <div class="form-group">
-                            <label for="">별도_영업이익</label>
+                            <label for="">별도_영업이익 <p style="display: inline-block;font-size: 70%;">(단위: 백만원)</p></label>
                             <input type="text" class="form-control" id="separate_operating_income" name="separate_operating_income" value="{{$info->separate_operating_income ?? ''}}">
                         </div>
                         <div class="form-group">
-                            <label for="">별도_당기순이익</label>
+                            <label for="">별도_당기순이익 <p style="display: inline-block;font-size: 70%;">(단위: 백만원)</p></label>
                             <input type="text" class="form-control" id="separate_net_income" name="separate_net_income" value="{{$info->separate_net_income ?? ''}}">
                         </div>
                         <div class="form-group">
-                            <label for="">별도_자산</label>
-                            <input type="text" class="form-control" id="separate_assets" name="separate_assets" value="{{$info->separate_assets ?? ''}}">
+                            <label for="">별도_자산 <p style="display: inline-block;font-size: 70%;">(단위: 백만원)</p></label>
+                            <input type="text" class="form-control" id="separate_assets" name="separate_assets" value="{{$info->separate_assets ?? ''}}" numberOnly>
                         </div>
                         <div class="form-group">
-                            <label for="">별도_부채</label>
-                            <input type="text" class="form-control" id="separate_liability" value="{{$info->separate_liability ?? ''}}">
+                            <label for="">별도_부채 <p style="display: inline-block;font-size: 70%;">(단위: 백만원)</p></label>
+                            <input type="text" class="form-control" id="separate_liability" value="{{$info->separate_liability ?? ''}}" numberOnly>
                         </div>
                     </div>
                 </div>
@@ -96,10 +96,11 @@
             //저장 버튼 클릭시
             $('.save-btn').on('click', function() {
                 if (confirm('저장하시겠습니까?')) {
-                    $('.info-form').submit();
+                        validation();
+                        $('.info-form').submit();
+
                 }
             });
-
 
         }
         const numeric_chk = () => {
@@ -107,14 +108,28 @@
             // 숫자만
             $('#connect_sales, #connect_operating_income, #connect_net_income, #connect_assets, #connect_liability').on('input', function(e){
                 this.value = this.value.replace(/[^0-9]/g, '');
-                this.value = this.value.slice(0, '11');
+                this.value = this.value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                this.value = this.value.slice(0, '13');
             });
 
             $('#separate_sales, #separate_operating_income, #separate_net_income, #separate_assets, #separate_liability').on('input', function(e){
                 this.value = this.value.replace(/[^0-9]/g, '');
-                this.value = this.value.slice(0, '11');
+                this.value = this.value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                this.value = this.value.slice(0, '13');
             });
 
+        }
+        const validation = () => {
+            $('#connect_sales').val($('#connect_sales').val().replaceAll(',', ''));
+            $('#connect_operating_income').val($('#connect_operating_income').val().replaceAll(',', ''));
+            $('#connect_net_income').val($('#connect_net_income').val().replaceAll(',', ''));
+            $('#connect_assets').val($('#connect_assets').val().replaceAll(',', ''));
+            $('#connect_liability').val($('#connect_liability').val().replaceAll(',', ''));
+            $('#separate_sales').val($('#separate_sales').val().replaceAll(',', ''));
+            $('#separate_operating_income').val($('#separate_operating_income').val().replaceAll(',', ''));
+            $('#separate_net_income').val($('#separate_net_income').val().replreplaceAllace(',', ''));
+            $('#separate_assets').val($('#separate_assets').val().replaceAll(',', ''));
+            $('#separate_liability').val($('#separate_liability').val().replaceAll(',', ''));
         }
 
         init();
