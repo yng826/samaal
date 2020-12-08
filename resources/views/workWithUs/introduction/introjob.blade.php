990b187f2d83a5584fd2d29fd5971981baf83ded
akaming <audals2478@gmail.com>
2020-12-09 01:23:39 +0900

    직무소개 페이지 작업

--- resources/views/workWithUs/introduction/introjob.blade.php
+++ resources/views/workWithUs/introduction/introjob.blade.php
@@ -17,7 +17,7 @@
     <div class="introduction-introjob__contents">
         <ul class="introduction-introjob__list">
             <li class="introduction-introjob__list--item">
-                <h3>생산</h3>
+                <h3 class="title-production">생산</h3>
                 <div class="content-box">
                     <ul>
                         <li>
@@ -29,12 +29,12 @@
                             생산일정에 따라 목표로 하는 제품을 생산한다.
                         </li>
                     </ul>
-                    <span>소속팀 : 생산팀, 생산관리팀</span>
-                    <a href="#">직무 인터뷰 보기</a>
+                    <span class="text-team">소속팀 : 생산팀, 생산관리팀</span>
+                    <a href="#" class="btn-link">직무 인터뷰 보기</a>
                 </div>
             </li>
             <li class="introduction-introjob__list--item">
-                <h3>영업</h3>
+                <h3 class="title-sales">영업</h3>
                 <div class="content-box">
                     <ul>
                         <li>
@@ -49,12 +49,12 @@
                             판매 실적 및 원가 북석을 통해 영업 전략을 수립, 실행한다.
                         </li>
                     </ul>
-                    <span>소속팀 : 국내영업팀, 해외영업팀, 부산사무소, 영업관리팀</span>
-                    <a href="#">직무 인터뷰 보기</a>
+                    <span class="text-team">소속팀 : 국내영업팀, 해외영업팀, 부산사무소, 영업관리팀</span>
+                    <a href="#" class="btn-link">직무 인터뷰 보기</a>
                 </div>
             </li>
             <li class="introduction-introjob__list--item">
-                <h3>품질보증</h3>
+                <h3 class="title-warranty">품질보증</h3>
                 <div class="content-box">
                     <ul>
                         <li>
@@ -70,8 +70,112 @@
                             개선해 나간다.
                         </li>
                     </ul>
-                    <span>소속팀 : 품질관리팀, 품질환경사무국</span>
-                    <a href="#">직무 인터뷰 보기</a>
+                    <span class="text-team">소속팀 : 품질관리팀, 품질환경사무국</span>
+                    <a href="#" class="btn-link">직무 인터뷰 보기</a>
+                </div>
+            </li>
+            <li class="introduction-introjob__list--item">
+                <h3 class="title-businessSupport">경영지원</h3>
+                <div class="content-box">
+                    <ul class="mb-7">
+                        <li>
+                            전사 경영계획수립, 투자계획수립, 원가관리, 감사업무를 수행한다.
+                        </li>
+                        <li>
+                            인적 자원의 효율적 운영을 위한 인사전략을 수립하고,
+                            인재 양성을 위한 교육 훈련 프로그램을 계획하고 운영한다.
+                        </li>
+                        <li>
+                            기업 재무 활동에 필요한 자금을 조달, 운용, 집행, 관리하며,
+                            재무 상태와 경영성과를 명확히 하기 위해 투명성 있는 회계를 실시한다.
+                        </li>
+                        <li>
+                            전사 경영계획수립, 투자계획수립, 원가관리, 감사업무를 수행한다.
+                        </li>
+                    </ul>
+                    <span class="text-team">소속팀 : 기획감사팀, 총무팀, 경리팀, 구매팀</span>
+                    <a href="#" class="btn-link">직무 인터뷰 보기</a>
+                </div>
+            </li>
+            <li class="introduction-introjob__list--item">
+                <h3 class="title-productionSupport">생산지원</h3>
+                <div class="content-box">
+                    <ul>
+                        <li>
+                            원자재, 부자재, 제품 등의 조달·입고·보관을 효율적으로
+                            수행하여 원활한 제품생산에 기여하며, 제품을 적기에
+                            고객에게 공급한다.
+                        </li>
+                        <li>
+                            최적의 생산이 가능하도록 공장을 운영하며, 공장 인사 및
+                            노사관계에 힘쓴다.
+                        </li>
+                    </ul>
+                    <span class="text-team">소속팀: 관리팀, 노무팀</span>
+                    <a href="#" class="btn-link">직무 인터뷰 보기</a>
+                </div>
+            </li>
+            <li class="introduction-introjob__list--item">
+                <h3 class="title-rnd">R&D</h3>
+                <div class="content-box">
+                    <ul>
+                        <li>
+                            신제품 개발로 신성장 동력을 발굴하여 고부가가치를 창출한다.
+                        </li>
+                        <li>
+                            신소재 개발 및 공정 개선을 통하여 원가절감, 생산성 향상,
+                            제품 품질 향상을 추진한다.
+                        </li>
+                    </ul>
+                    <span class="text-team">소속팀: 생산기술팀, 연구개발팀</span>
+                    <a href="#" class="btn-link">직무 인터뷰 보기</a>
+                </div>
+            </li>
+            <li class="introduction-introjob__list--item">
+                <h3 class="title-equipment">>설비</h3>
+                <div class="content-box">
+                    <ul>
+                        <li>
+                            설비 도입 및 개조, 개선, 유지관리로 생산 능력을 확보하고
+                            생산성 향상에 기여한다.
+                        </li>
+                        <li>
+                            설비의 효율적 운영 및 작업환경개선을 위한 점검, 수리,
+                            개조개선에 대한 계획수립 및 작업을 진행한다.
+                        </li>
+                    </ul>
+                    <span class="text-team">소속팀: 설비기술팀, 관리기술팀, 보전팀</span>
+                    <a href="#" class="btn-link">직무 인터뷰 보기</a>
+                </div>
+            </li>
+            <li class="introduction-introjob__list--item">
+                <h3 class="title-safety">환경안전</h3>
+                <div class="content-box">
+                    <ul>
+                        <li>
+                            환경, 안전, 보건, 소방 관련 법규 준수를 통해 쾌적하고
+                            안전한 사업장을 형성한다
+                        </li>
+                    </ul>
+                    <span class="text-team">소속팀 : 환경안전팀</span>
+                    <a href="#" class="btn-link">직무 인터뷰 보기</a>
+                </div>
+            </li>
+            <li class="introduction-introjob__list--item">
+                <h3 class="title-it">IT</h3>
+                <div class="content-box">
+                    <ul>
+                        <li>
+                            정보시스템의 효율적 운영을 위한 운영, 보안, 유지보수,
+                            자원관리 등을 실시한다.
+                        </li>
+                        <li>
+                            체계화되고 검증된 시스템 구축 및 운영 프로세스를 통한
+                            안정적인 IT 환경을 유지한다.
+                        </li>
+                    </ul>
+                    <span class="text-team">소속팀 : 환경안전팀</span>
+                    <a href="#" class="btn-link">직무 인터뷰 보기</a>
                 </div>
             </li>
         </ul>
