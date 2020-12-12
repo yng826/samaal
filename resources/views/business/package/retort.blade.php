38cd3e3ac5ef8b361e3eeedb5efc3e4156a2bec7
akaming <audals2478@gmail.com>
2020-12-12 12:48:27 +0900

    팝업 반응형

--- resources/views/business/package/retort.blade.php
+++ resources/views/business/package/retort.blade.php
@@ -11,6 +11,7 @@
 @section('swiper-container')
     <div class="swiper-slide"><div class="slide-img" style="background-image:url(/images/business/foil/img_retort.jpg);"></div></div>
     <div class="swiper-slide"><div class="slide-img" style="background-image:url(/images/business/foil/img_retort_hover.jpg);"></div></div>
+    <div class="swiper-slide"><div class="slide-img" style="background-image:url(/images/business/foil/img_retort_hover02.jpg);"></div></div>
 @endsection
 
 @section('info__title')
