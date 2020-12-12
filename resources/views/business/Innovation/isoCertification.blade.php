e2e43552d2e069d759f3671804d248ddc0038503
akaming <audals2478@gmail.com>
2020-12-12 18:29:21 +0900

    인증현황 태블릿 모바일 작업

--- resources/views/business/Innovation/isoCertification.blade.php
+++ resources/views/business/Innovation/isoCertification.blade.php
@@ -12,6 +12,144 @@
             </h2>
         </div>
         <div class="speciality-iso__section">
+            <div class="speciality-iso__table">
+                <h3>품질경영 시스템</h3>
+                <table>
+                    <colgroup>
+                        <col width="30%">
+                        <col width="25%">
+                        <col width="auto">
+                    </colgroup>
+                    <tr>
+                        <th>인증규격</th>
+                        <th>인증번호</th>
+                        <th>최초인증일</th>
+                    </tr>
+                    <tr>
+                        <td>ISO 9001 (국문)</td>
+                        <td>QMS-2956</td>
+                        <td>
+                            31995.10.20
+                            <div class="file-box">
+                                <a href="#" class="btn-download">PDF파일</a>
+                                <a href="#" class="btn-preview">미리보기</a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>ISO 9001 (국문)</td>
+                        <td>QMS-2956</td>
+                        <td>
+                            31995.10.20
+                            <div class="file-box">
+                                <a href="#" class="btn-download">PDF파일</a>
+                                <a href="#" class="btn-preview">미리보기</a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>ISO 9001 (국문)</td>
+                        <td>QMS-2956</td>
+                        <td>
+                            31995.10.20
+                            <div class="file-box">
+                                <a href="#" class="btn-download">PDF파일</a>
+                                <a href="#" class="btn-preview">미리보기</a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>ISO 9001 (국문)</td>
+                        <td>QMS-2956</td>
+                        <td>
+                            31995.10.20
+                            <div class="file-box">
+                                <a href="#" class="btn-download">PDF파일</a>
+                                <a href="#" class="btn-preview">미리보기</a>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="speciality-iso__table">
+                <h3>환경경영 시스템</h3>
+                <table>
+                    <colgroup>
+                        <col width="30%">
+                        <col width="25%">
+                        <col width="auto">
+                    </colgroup>
+                    <tr>
+                        <th>인증규격</th>
+                        <th>인증번호</th>
+                        <th>최초인증일</th>
+                    </tr>
+                    <tr>
+                        <td>ISO 9001 (국문)</td>
+                        <td>QMS-2956</td>
+                        <td>
+                            31995.10.20
+                            <div class="file-box">
+                                <a href="#" class="btn-download">PDF파일</a>
+                                <a href="#" class="btn-preview">미리보기</a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>ISO 9001 (국문)</td>
+                        <td>QMS-2956</td>
+                        <td>
+                            31995.10.20
+                            <div class="file-box">
+                                <a href="#" class="btn-download">PDF파일</a>
+                                <a href="#" class="btn-preview">미리보기</a>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="speciality-iso__table">
+                <h3>식품 안전 경영시스템</h3>
+                <table>
+                    <colgroup>
+                        <col width="30%">
+                        <col width="25%">
+                        <col width="auto">
+                    </colgroup>
+                    <tr>
+                        <th>인증규격</th>
+                        <th>인증번호</th>
+                        <th>최초인증일</th>
+                    </tr>
+                    <tr>
+                        <td>ISO 9001 (국문)</td>
+                        <td>QMS-2956</td>
+                        <td>
+                            31995.10.20
+                            <div class="file-box">
+                                <a href="#" class="btn-download">PDF파일</a>
+                                <a href="#" class="btn-preview">미리보기</a>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>ISO 9001 (국문)</td>
+                        <td>QMS-2956</td>
+                        <td>
+                            31995.10.20
+                            <div class="file-box">
+                                <a href="#" class="btn-download">PDF파일</a>
+                                <a href="#" class="btn-preview">미리보기</a>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+
+
             <table class="speciality-iso__table">
                 <tr>
                     <th>구분</th>
