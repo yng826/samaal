3e825f1c68aa983d269a324ebe4be148ae95e401
hyuk <yng826@gmail.com>
2020-12-14 03:07:43 +0900

    사이트맵 table->ul li

--- resources/views/other/sitemap.blade.php
+++ resources/views/other/sitemap.blade.php
@@ -9,18 +9,16 @@
         </div>
         <div class="contents-wrap__section sitemap__section">
 
-            <table>
+            <ul>
             @foreach ($sitemaps as $sitemap)
-                <tr>
-                    <td class="depth-01">
-                        @if (strlen($sitemap->url) > 2)
-                            <a href="{{ $sitemap->url }}">{{ $sitemap->name }}</a>
-                        @else
-                            {{ $sitemap->name }}
-                        @endif
-                    </td>
+                <li class="depth-01">
+                    @if (strlen($sitemap->url) > 2)
+                        <a href="{{ $sitemap->url }}">{{ $sitemap->name }}</a>
+                    @else
+                        {{ $sitemap->name }}
+                    @endif
+                    <div class="container container-left">
                     @for ($i = 0; $i < $depth2Cnt; $i++)
-                        <td>
                         @if (isset($sitemap->children[$i]))
                             @if (strlen($sitemap->children[$i]->url) > 2)
                                 <a href="{{ $sitemap->children[$i]->url }}"><b class="depth-02">{{ $sitemap->children[$i]->name }}</b></a>
@@ -36,11 +34,11 @@
                             @endif
                             </ul>
                         @endif
-                        </td>
                     @endfor
-                </tr>
+                    </div>
+                </li>
             @endforeach
-            </table>
+            </ul>
         </div>
     </main>
 @endsection
