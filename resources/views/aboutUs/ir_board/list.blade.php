d7b7d4c6d6586c567a91b149cf60396f971291c9
akaming <audals2478@gmail.com>
2020-12-14 23:43:14 +0900

    재무제표 전자공고 수정

--- resources/views/aboutUs/ir_board/list.blade.php
+++ resources/views/aboutUs/ir_board/list.blade.php
@@ -21,68 +21,70 @@
                    financial
                 </h2>
             </div>
-            <div class="ir-wrap--tab">
-                <ul>
-                    <li class="tab-item {{$id =='consolidated' ? 'on': ''}}">
-                        <a href="/about-us/ir/consolidated">연결재무제표</a>
-                    </li>
-                    <li class="tab-item {{$id =='separate' ? 'on': ''}}">
-                        <a href="/about-us/ir/separate">별도재무제표</a></li>
-                    </li>
-                    <li class="tab-item {{$id =='board' ? 'on': ''}}">
-                        <a href="/about-us/ir/board">전자공고</a></li>
-                    </li>
-                </ul>
-            </div>
-            <div class="contents-wrap__section" id="about-ir__board">
-                <h3>전자공고</h3><br />
-                <table class="table">
-                    <colgroup>
-                        <col width="10%">
-                        <col width="auto">
-                        <col width="15%">
-                      </colgroup>
-                    <thead>
-                        <tr>
-                            <th class="text-center .about-ir__no">번호</th>
-                            <th class="text-center">제목</th>
-                            <th class="text-center">등록/수정일</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($ir_boards as $ir_board)
-                        <tr>
-                            <td class="text-center">{{ $ir_board->id }}</td>
-                            <td class="text-center .about-ir__fixed"><div class="ir-contents"> <a class="btn btn-outline-info btn-xs" href="/about-us/ir/board/{{$ir_board->id}}">{{ $ir_board->title }}</a></div></td>
-                            <td class="text-center">{{ $ir_board->updated_at ?? $ir_board->created_at}}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <br />
-            <div class="pagination">
-                <a href="/about-us/ir/board?page=1" class="pagination__button-prev"></a>
-                @for($i=1; $i<$cnt+1; $i++)
-                <?php
-                    $style='';
-                    ?>
-                 @if(!isset($_GET['page']))
-                   <?php
-                        $_GET['page'] = 1;
-                   ?>
-                 @endif
-                    @if ($_GET['page'] == $i)
+            <div class="about-ir-list__section">
+                <div class="ir-wrap--tab">
+                    <ul>
+                        <li class="tab-item {{$id =='consolidated' ? 'on': ''}}">
+                            <a href="/about-us/ir/consolidated">연결재무제표</a>
+                        </li>
+                        <li class="tab-item {{$id =='separate' ? 'on': ''}}">
+                            <a href="/about-us/ir/separate">별도재무제표</a></li>
+                        </li>
+                        <li class="tab-item {{$id =='board' ? 'on': ''}}">
+                            <a href="/about-us/ir/board">전자공고</a></li>
+                        </li>
+                    </ul>
+                </div>
+                <div class="about-ir-list__table" id="about-ir__board">
+                    <h3>전자공고</h3><br />
+                    <table class="table">
+                        <colgroup>
+                            <col width="10%">
+                            <col width="auto">
+                            <col width="20%">
+                          </colgroup>
+                        <thead>
+                            <tr>
+                                <th class="text-center .about-ir__no">번호</th>
+                                <th class="text-center">제목</th>
+                                <th class="text-center">등록/수정일</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($ir_boards as $ir_board)
+                            <tr>
+                                <td class="text-center">{{ $ir_board->id }}</td>
+                                <td class="text-center .about-ir__fixed"><div class="ir-contents"> <a class="btn btn-outline-info btn-xs" href="/about-us/ir/board/{{$ir_board->id}}">{{ $ir_board->title }}</a></div></td>
+                                <td class="text-center">{{ $ir_board->updated_at ?? $ir_board->created_at}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <br />
+                <div class="pagination">
+                    <a href="/about-us/ir/board?page=1" class="pagination__button-prev"></a>
+                    @for($i=1; $i<$cnt+1; $i++)
                     <?php
-                        $style='color: blue';
-                    ?>
-                    @endif
-                    <a href="/about-us/ir/board?page={{ $i }}" class="pagination__number"style="{{ $style }}">{{ $i }}</a>
-                @endfor
-                <a href="/about-us/ir/board?page={{ $cnt }}" class="pagination__button-next"></a>
-            </div>
-            <div class="about-ir__link">
-                <button type="button" onclick="location.href='#' ">DART 바로가기</button>
+                        $style='';
+                        ?>
+                     @if(!isset($_GET['page']))
+                       <?php
+                            $_GET['page'] = 1;
+                       ?>
+                     @endif
+                        @if ($_GET['page'] == $i)
+                        <?php
+                            $style='color: blue';
+                        ?>
+                        @endif
+                        <a href="/about-us/ir/board?page={{ $i }}" class="pagination__number"style="{{ $style }}">{{ $i }}</a>
+                    @endfor
+                    <a href="/about-us/ir/board?page={{ $cnt }}" class="pagination__button-next"></a>
+                </div>
+                <div class="about-ir__link">
+                    <button type="button" onclick="location.href='#' ">DART 바로가기</button>
+                </div>
             </div>
         </main>
 
