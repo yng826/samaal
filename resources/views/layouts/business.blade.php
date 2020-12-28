fb18a5321cbf63474030c85c9841c18e8938636a
hyuk <yng826@gmail.com>
2020-12-29 01:04:55 +0900

    fix

--- resources/views/layouts/business.blade.php
+++ resources/views/layouts/business.blade.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>{{ config('app.name', '삼아알미늄') }}</title>
-        {{-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> --}}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="/css/app.css">
     </head>
