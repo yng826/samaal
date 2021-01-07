0ddb21dc8e090135887d86656906b0e933f54d5d
akaming <audals2478@gmail.com>
2021-01-07 19:51:12 +0900

    sns 이미지 추가

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
-
+    <meta property="og:image" content="{{APP_URL }}/public/img_sns_sama.png" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
--- resources/views/layouts/sub.blade.php
+++ resources/views/layouts/sub.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>{{ config('app.name', '삼아알미늄') }}</title>
+        <meta property="og:image" content="{{APP_URL }}/public/img_sns_sama.png" />
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
