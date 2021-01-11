3b6b23f2a9ddcc09af508a158ed3d5b442c5fc70
hyuk <yng826@gmail.com>
2021-01-11 23:11:17 +0900

    이름 입력 전에는 disabled

--- resources/js/components/job/IndividualInfo.vue
+++ resources/js/components/job/IndividualInfo.vue
@@ -22,14 +22,14 @@
                 <h3>인적사항</h3>
                 <div class="form-group">
                     <label for="">성명(한글)</label>
-                    <input type="text" name="name" v-model="user.name" placeholder="입력해주세요.">
+                    <input type="text" name="name" v-model="user.name" placeholder="입력해주세요." />
                 </div>
                 <div class="form-group">
                     <label for="">성명(영문)</label>
-                    <input type="text" name="name_en" v-model="user_info.name_en" placeholder="입력해주세요.">
+                    <input type="text" name="name_en" v-model="user_info.name_en" placeholder="입력해주세요." />
                 </div>
                 <div class="form-group">
-                    <label for="">생년월일</label>
+                    <label for="">생년월일{{ inputMaskEnable }}</label>
                     <InputMask type="text" :disabled="inputMaskEnable" name="birth_day" mask="9999-99-99" v-model="user_info.birth_day" maskChar=" " placeholder="입력해주세요."/>
                 </div>
                 <div class="form-group">
@@ -39,9 +39,9 @@
                 <div class="form-group">
                     <label for="">E-MAIL</label>
                     <div class="input-group">
-                        <input type="text" name="email" :disabled="job.id || isAuth" :value="mode == 'edit' ? this.email : this.createEmail" @input="onInputEmail" placeholder="입력해주세요.">
+                        <input type="text" name="email" :disabled="job.id || isAuth" :value="mode == 'edit' ? this.email : this.createEmail" @input="onInputEmail" placeholder="입력해주세요." />
                         <span v-if="emailEditable">@</span>
-                        <input type="text" name="email_vendor_input" :value="this.emailVendor" @input="onInputVendor" :readonly="!selectEdit" v-if="emailEditable" placeholder="입력 또는 선택해주세요.">
+                        <input type="text" name="email_vendor_input" :value="this.emailVendor" @input="onInputVendor" :readonly="!selectEdit" v-if="emailEditable" placeholder="입력 또는 선택해주세요." />
                         <div class="select-container" v-if="emailEditable">
                             <select name="email_vendor_select" id="" @change="onSelectEmail">
                                 <option value="">이메일</option>
@@ -58,18 +58,18 @@
                 </div>
                 <div class="form-group">
                     <label for="password">비밀번호</label>
-                    <input type="password" name="password" id="password" v-model="password" ref="password" placeholder="8자이상 영문/숫자/기호 조합">
+                    <input type="password" name="password" id="password" v-model="password" ref="password" placeholder="8자이상 영문/숫자/기호 조합" />
                 </div>
                 <div class="form-group">
                     <label for="password_confirm">비밀번호재입력</label>
-                    <input type="password" name="password_confirm" id="password_confirm" v-model="password_confirm" ref="password_confirm" placeholder="8자이상 영문/숫자/기호 조합">
+                    <input type="password" name="password_confirm" id="password_confirm" v-model="password_confirm" ref="password_confirm" placeholder="8자이상 영문/숫자/기호 조합" />
                 </div>
                 <div class="form-group">
                     <label for="">현거주지</label>
                     <div class="input-group">
-                        <input type="text" name="address_1" v-model="job.address_1" placeholder="검색해주세요." @click="onClickPost" readonly>
+                        <input type="text" name="address_1" v-model="job.address_1" placeholder="검색해주세요." @click="onClickPost" readonly />
                         <DaumPost style="" @complete="onSearch" v-show="showPost" />
-                        <input type="text" name="address_2" v-model="job.address_2" placeholder="입력해주세요.">
+                        <input type="text" name="address_2" v-model="job.address_2" placeholder="입력해주세요." />
                     </div>
                 </div>
             </div>
@@ -110,7 +110,7 @@
                     </ul>
                 </div>
                 <label for="check-1" class="information-box__label">
-                    <input type="checkbox" name="" id="check-1" class="information-box__check" v-model="agree">
+                    <input type="checkbox" name="" id="check-1" class="information-box__check" v-model="agree" />
                     <span>개인정보 이용 및 수집에 동의 합니다.</span>
                 </label>
                 <!-- <label for="check-2" class="information-box__label">
@@ -181,7 +181,11 @@ export default {
     computed: {
         isOpen() { return this.$store.state.recruit_status == 'open' || this.mode =='create' },
         inputMaskEnable() {
-            return this.user.name == undefined ? 'disabled': false;
+            if (this.user.name == undefined || this.user.name == '') {
+                return true;
+            } else {
+                return false;
+            }
         },
         email() {
             if ( this.mode == 'create' ) {
