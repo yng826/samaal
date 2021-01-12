9c812c6e0ee4ef95e19e4cd9da558129d7d36e52
akaming <audals2478@gmail.com>
2021-01-12 21:23:38 +0900

    email 수정

--- resources/views/layouts/managerPop02.blade.php
+++ resources/views/layouts/managerPop02.blade.php
@@ -10,10 +10,10 @@
     </dl>
     <dl>
         <dt>Tel</dt>
-        <dd><a href="tel:02-3458-0542">+82 2-3458-0562</a></dd>
+        <dd><a href="tel:02-3458-0562">+82 2-3458-0562</a></dd>
     </dl>
     <dl>
         <dt>E-mail</dt>
-        <dd><a href="mailto:kimsj@sama-al.com">kimsj@sama-al.com</a></dd>
+        <dd><a href="mailto:mhkim@sama-al.com">mhkim@sama-al.com</a></dd>
     </dl>
 </div>
