0ce1a4b2bddd8749ce2c67ae63042ad4309bae93
hyuk <yng826@gmail.com>
2021-02-03 21:22:25 +0900

    파비콘 경로수정

--- resources/views/layouts/business.blade.php
+++ resources/views/layouts/business.blade.php
@@ -11,7 +11,7 @@
         <meta property="og:id" content="sama">
         <meta property="og:image" content="{{env('APP_URL')}}/img_sns_sama.png" />
         <link rel="stylesheet" href="/kor/css/app.css">
-        <link rel="shortcut icon" href="{{env('APP_URL')}}kor/images/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="{{env('APP_URL')}}/images/favicon.ico" type="image/x-icon">
         <link rel="icon" href="/kor/images/favicon.ico" type="image/x-icon">
         @include('shared.gtm-header')
     </head>
--- resources/views/shared/notice.blade.php
+++ resources/views/shared/notice.blade.php
@@ -3,6 +3,7 @@
     @foreach ($notices as $notice)
     <div class="notice-group main-modal__wrap">
         <h3 class="main-modal__title">{{$notice->title}}</h3>
+        <p class="main-modal__content">{{$notice->content}}</p>
         <a href="{{$notice->url}}" style="background-color: #{{$notice->button_color}}" class="main-modal__link">바로가기</a>
     </div>
     @endforeach
