645b74368118ec5368b78184c088876eb4841720
yng826 <yng826@gmail.com>
2021-04-11 20:21:40 +0900

    언어변경 클릭시 오류 수정

--- resources/js/common.js
+++ resources/js/common.js
@@ -4,7 +4,8 @@ const common = () => {
         const $lang = $('.header .language__kor');
         const $lang_en = $('.header .language__eng');
 
-        $lang.on("click",function(){
+        $lang.on("click",function(e){
+            e.preventDefault();
             $lang_en.toggleClass('on');
             $lang.toggleClass('on');
         });
